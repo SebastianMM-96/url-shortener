56dc00be67fcc83764a2b6991ea468d5f129128d
Sebastian Marroquin <sebasmarro10@gmail.com>
2020-10-10 13:45:55 -0500

    URL Shortener using Django :snake:

---
+++ src/shortener/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class urlshortenerURL(models.Model):
+    url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return str(self.url)
+    
+    def __unicode__(self):
+        return str(self.url)
